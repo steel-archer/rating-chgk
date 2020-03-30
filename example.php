226342f45fd1461a4265dc0afcc6995e5284e436
Bohdan Sokulskyi <steelarcher@gnail.com>
2020-03-30 22:07:03 +0300

    moved example.php

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 $api = new \SteelArcher\RatingChgk\Api();
 $playerInfo = $api->getPlayers()->getPlayerInfo(178444);
