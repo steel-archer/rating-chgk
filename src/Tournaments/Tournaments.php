c9e98a5a8ddbd195b287ed803d26209fb5f471d0
Bohdan Sokulskyi <steelarcher@gnail.com>
2020-04-04 20:11:33 +0300

    Added some Tournaments endpoints.

--- src/Tournaments/Tournaments.php
+++ src/Tournaments/Tournaments.php
@@ -17,4 +17,35 @@ class Tournaments extends Resource
     {
         return 'tournaments/';
     }
+
+    /**
+     * @param int $page
+     * @return array
+     */
+    public function getTournaments(int $page = 1): array
+    {
+        $result = $this->get('?page=%d', [$page]);
+
+        return $result ? current($result) : [];
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function getTournamentInfo(int $id): array
+    {
+        $result = $this->get('%d', [$id]);
+
+        return $result ? current($result) : [];
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function getTournamentResults(int $id): array
+    {
+        return $this->get('%d/list', [$id]);
+    }
 }
